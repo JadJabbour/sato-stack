2637596e7733f215cc4a58a0276a119e41cc9e59
Jad Jabbour <jabbourj.87@gmail.com>
2021-04-09 07:05:06 +0300

    remove tesst code

--- ai/actions/test.py
+++
@@ -1,9 +0,0 @@
-from actions.celery import clapp, training_worker_queue
-
-@clapp.task(queue=training_worker_queue)
-def test():
-    import os
-
-    os.mkdir('madeithere')
-
-    return "im on redis"
