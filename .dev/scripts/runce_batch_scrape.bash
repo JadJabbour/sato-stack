13ddce14883631e2cd898976b686005ee2048e1e
Jad Jabbour <jabbourj.87@gmail.com>
2022-05-14 07:35:38 +0300

    batching

---
+++ .dev/scripts/runce_batch_scrape.bash
@@ -0,0 +1,6 @@
+bash runce_crypto_scraper.bash BTC 1000 &&\
+bash runce_crypto_scraper.bash ETH 1000 &&\
+bash runce_crypto_scraper.bash SOL 1000 &&\
+bash runce_crypto_scraper.bash ADA 1000 &&\
+bash runce_crypto_scraper.bash TRX 1000 &&\
+bash runce_crypto_scraper.bash XRP 1000
---
+++ .dev/scripts/runce_batch_train.bash
@@ -0,0 +1,6 @@
+bash runce_train.bash BTC &&\
+bash runce_train.bash ETH &&\
+bash runce_train.bash SOL &&\
+bash runce_train.bash ADA &&\
+bash runce_train.bash TRX &&\
+bash runce_train.bash XRP
